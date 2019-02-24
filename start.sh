7ebb24b4f81bcae1a24887df625608bb1306d38c
Nick Ayers <n1ckay3r5@gmail.com>
2019-02-24 17:43:57 +0000

    add bash kernel. change bind mount location so that jupyer config in home directory is available at run time.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # start local jupyter image on the 'elastic' network with working directory bind mount
 
-docker run -p 8888:8888 --network elastic -v ~/repos/python:/home/jovyan/:rw jupyter
+docker run --rm -p 8888:8888 --network elastic -v ~/repos/python:/home/jovyan/repos/:rw jupyter
